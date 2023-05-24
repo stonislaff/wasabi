6dbb6e17ea9712f2972dfc5db111d8ad6333ac30
stonislaff <yanoter1232003@gmail.com>
2023-05-25 00:26:14 +0300

    -reworked main page -menu page bg added

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,25 +1,32 @@
 import './MainPage.scss'
-import sushi from '../../assets/sushi.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
-import Carousel from 'react-bootstrap/Carousel';
+import gps from '../../assets/gps.png'
+import phone from '../../assets/phone.png'
 function MainPage(){
         return(
             <div className="MainPage">
-                <Carousel touch="False">
-                    <Carousel.Item>
-                        <img src={sushi}/>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={sushi}/>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={sushi}/>
-                        <Carousel.Caption>
-                            <Button variant="outline-warning">Замовити</Button>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
+                <div className="topHalf"></div>
+                <div className="bottomHalf"></div>
+                <div className="infoBlock">
+                    <div className="label">
+                        Втілюй смак у життя разом з нами
+                    </div>
+                    <div className="infoLine">
+                        <img src={gps}/>Кулика і Гудачека 44
+                        <h1>|</h1>
+                        <img src={phone}/>068-375-07-10
+                    </div>
+                    <div className="infoLine">
+                        <img src={gps}/>Соборна 338
+                        <h1>|</h1>
+                        <img src={phone}/>063-273-33-02
+                    </div>
+                    <div className="infoLine">
+                        <img src={gps}/>Князя Романа 4
+                        <h1>|</h1>
+                        <img src={phone}/>096-683-23-88
+                    </div>
+                </div>
             </div>
         )
 }
--- src/pages/menuPage/MenuPage.jsx
+++ src/pages/menuPage/MenuPage.jsx
@@ -1,10 +1,48 @@
 import './menuPage.scss'
+import Nav from 'react-bootstrap/Nav';
+import React, { useState } from 'react';
 
 function MenuPage(){
+    const [buttonClasses, setButtonClasses] = useState({
+        button1: 'menuHeaderButtonActive',
+        button2: 'menuHeaderButton',
+        button3: 'menuHeaderButton',
+        button4: 'menuHeaderButton',
+        button5: 'menuHeaderButton'
+    });
+    const changeButtonClass = (buttonName) => {
+        setButtonClasses(prevClasses => {
+            const updatedClasses = {};
+            updatedClasses[buttonName] = "menuHeaderButtonActive";
+            Object.keys(prevClasses).forEach(name => {
+                if (name !== buttonName) {
+                    updatedClasses[name] = 'menuHeaderButton';
+                }
+            });
+
+            return updatedClasses;
+        });
+    };
+
     return(
         <div className="MenuPage">
+            <div className="topHalf"></div>
+            <div className="bottomHalf"></div>
+            <div className="contentLine">
+                <div className="menuBlock">
+                    <div className="menuHeader">
+                        <button className={buttonClasses.button1} onClick={() => changeButtonClass('button1')}>Суші</button>
+                        <button className={buttonClasses.button2} onClick={() => changeButtonClass('button2')}>Піца</button>
+                        <button className={buttonClasses.button3} onClick={() => changeButtonClass('button3')}>Напої</button>
+                        <button className={buttonClasses.button4} onClick={() => changeButtonClass('button4')}>Бургери</button>
+                        <button className={buttonClasses.button5} onClick={() => changeButtonClass('button5')}>Страви на компанію</button>
+                    </div>
+                    <div className="menuBody">
 
-    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
