2fa6833a168f04829a19f583657fdadcfc39d491
stonislaff <yanoter1232003@gmail.com>
2023-06-08 16:27:15 +0300

    -many updates

--- src/pages/ordersPages/mainOrderPage/StartOrder.jsx
+++ src/pages/ordersPages/mainOrderPage/StartOrder.jsx
@@ -5,6 +5,7 @@ import FirstOrderPage from "./firstOrderPage/firstOrderPage";
 import SecondOrderPage from "./secondOrderPage/secondOrderPage";
 import ThirdOrderPage from "./thirdOrderPage/thirdOrderPage";
 import GenerateOrderNumber from "./generateOrderNumber";
+import axios from 'axios';
 
 const orderNumber = GenerateOrderNumber;
 
@@ -17,6 +18,7 @@ function StartOrder(){
     };
 
     const handleButtonClickSecondPage = () => {
+        axios.post('http://localhost:5000/send-message', { message: 'Hello from React!' })
         localStorage.clear()
         setShowThirdPage(true);
     };
---
+++ src/telegramModule/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const TelegramBot = require('node-telegram-bot-api');
+const cors = require('cors');
+const botToken = '6172729665:AAHFPE_VqLqCSJ5RpWoMSm1weSc5XUWuOPA';
+const channelId = '-902308119';
+
+const bot = new TelegramBot(botToken, { polling: true });
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post('/send-message', (req, res) => {
+    const message = req.body.message;
+
+    bot.sendMessage(channelId, message)
+        .then(() => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.error('Error sending message:', error);
+            res.sendStatus(500);
+        });
+});
+const port = 5000;
+
+app.listen(port, () => {
+    console.log('Server started on port 5000');
+});
