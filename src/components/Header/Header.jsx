df1bb089ecaecc5d50be0d31cb1ebe518eb6cca4
stonislaff <yanoter1232003@gmail.com>
2023-06-12 19:41:35 +0300

    -many updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,59 +1,21 @@
-import React, { useState } from 'react';
-import './Header.scss';
-import { NavLink } from 'react-router-dom';
-
-function Header() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const handleMenuToggle = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-
-    return (
+import './Header.scss'
+import {NavLink} from "react-router-dom";
+function Header(){
+    return(
         <div className="Header">
             <nav className="navBar">
                 <div className="logo">
                     <div className="logoText">
-            <span className="firstText">
-              <NavLink to="/">Wasabi</NavLink>
-            </span>
-                        <span className="secondText">
-              <NavLink to="/">sushi & pizza</NavLink>
-            </span>
+                            <span className="firstText"><NavLink to="/">Wasabi</NavLink></span>
+                        <span className="secondText"><NavLink to="/">sushi & pizza</NavLink></span>
                     </div>
                 </div>
-
-                {/* Button for toggling the dropdown menu */}
-                <div className="menuToggle" onClick={handleMenuToggle}>
-                    <i className={`fas ${isMenuOpen ? 'fa-times' : 'fa-bars'}`}></i>
-                </div>
-
-                {/* Dropdown menu */}
-                <div className={`dropdownMenu ${isMenuOpen ? 'open' : ''}`}>
-                    <div className="navElement">
-                        <NavLink to="/menu">Меню</NavLink>
-                    </div>
-                    <div className="navElement">
-                        <NavLink to="/order-delivery">Зробити замовлення</NavLink>
-                    </div>
-                    <div className="navElement">
-                        <NavLink to="/promotions">Акції</NavLink>
-                    </div>
-                </div>
-
-                {/* Regular navigation elements */}
-                <div className="navElement">
-                    <NavLink to="/menu">Меню</NavLink>
-                </div>
-                <div className="navElement">
-                    <NavLink to="/order-delivery">Зробити замовлення</NavLink>
-                </div>
-                <div className="navElement">
-                    <NavLink to="/promotions">Акції</NavLink>
-                </div>
+                <div className="navElement"><NavLink to="/menu">Меню</NavLink></div>
+                <div className="navElement"><NavLink to="/order-delivery">Зробити замовлення</NavLink></div>
+                <div className="navElement"><NavLink to="/promotions">Акції</NavLink></div>
             </nav>
         </div>
-    );
+    )
 }
 
-export default Header;
+export default Header
